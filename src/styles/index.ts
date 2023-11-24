0424355406ce46913ac36c1160b22236f96974e9
Yegorich555 <yegorich555@gmail.com>
2023-11-24 15:12:10 +0200

    Styles. Add default scroll for body

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,6 @@
-import { useBuiltinStyle, WUPcssBtnIcon } from "web-ui-pack/styles";
+import { useBuiltinStyle, WUPcssBtnIcon, WUPcssScrollSmall } from "web-ui-pack/styles";
 import "./main.scss";
 import styles from "./shared.m.scss";
 
 useBuiltinStyle(WUPcssBtnIcon(`.${styles.wupBtnIcon}`)); // re-use styles from package
+useBuiltinStyle(WUPcssScrollSmall("body"));
