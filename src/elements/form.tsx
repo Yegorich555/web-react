c6dc500b27bb7c71c9e1a065fe624aff7a71c4f2
Yegorich555 <yegorich555@gmail.com>
2023-11-29 13:31:44 +0200

    Buttons. Fix hover & focus style for unique buttons

--- src/elements/form.tsx
+++ src/elements/form.tsx
@@ -1,6 +1,5 @@
 import { WUPFormElement } from "web-ui-pack";
 import BaseWUP from "./baseWUP";
-// import styles from "./form.scss";
 
 WUPFormElement.$use(); // register control in the browser
 WUPFormElement.$defaults.autoStore = true;
