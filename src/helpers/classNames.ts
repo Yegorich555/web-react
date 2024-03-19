37f7c7d0f512778cb353586401d44e6e179f31a0
Yegor <yegorich555@gmail.com>
2024-03-19 11:38:53 +0200

    Helpers. classNames. Add usage details

--- src/helpers/classNames.ts
+++ src/helpers/classNames.ts
@@ -4,6 +4,7 @@ function cx(...args: TrueType[]): string {
   return args.filter((v) => v).join(" ");
 }
 
+// WARN: uncomment it if somehow function cx isn't globally visible despite on import into main.ts(x)
 // declare global {
 //   interface Window {
 //     cx: typeof cx;
